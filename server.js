9d4306b094795fdc9a50a639702f67956a74234e
Sarah <70615195+smanter82@users.noreply.github.com>
2021-02-24 22:50:48 -0500

    deploying

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgets", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
